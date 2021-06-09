973119ceeb4719e79895fc8eaf866d277a67d39f
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2021-06-09 08:21:04 -0500

    Reading constant and using string matching functions

--- nodejs-test/varConst-demo.js
+++ nodejs-test/varConst-demo.js
@@ -1,3 +1,10 @@
 const testConstant = 'Test Value of Constant';
-var testVar = 'Test Value of Variable';
-console.log('Value of append: ' + testConstant + '-' + testVar);
+var testVar = 'Test Value';
+console.log('Value of testConstant: ' + testConstant);
+let testVal = `${testVar} of`;
+console.log('Value of testVal: ', testVal);
+if(testConstant.includes(testVal)) {
+  console.log('Match found !');
+} else {
+  console.log('Match not found !');
+}
